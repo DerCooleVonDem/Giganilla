de6244f1e5fc56d5ab65dee165f68e0c376cc6b3
Jonas <jonaswindmann@gmail.com>
2024-03-24 18:03:15 +0100

    JungleEdge implemented

---
+++ src/generator/populator/JungleEdgePopulator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace JonasWindmann\Giganilla\generator\populator;
+
+use JonasWindmann\Giganilla\biome\BiomeList;
+use JonasWindmann\Giganilla\generator\populator\decorator\objects\BigOakTree;
+use JonasWindmann\Giganilla\generator\populator\decorator\objects\CocoaTree;
+
+class JungleEdgePopulator extends JunglePopulator {
+    public function initPopulators(): void {
+        $this->treeDecorator->setAmount(2);
+        $this->treeDecorator->setTrees([
+            [10, BigOakTree::class],
+            [45, CocoaTree::class]
+        ]);
+    }
+
+    public function getBiomes(): array {
+        return [BiomeList::JUNGLE_EDGE, BiomeList::JUNGLE_EDGE_MOUNTAINS];
+    }
+}
